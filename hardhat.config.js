1035ff8935732567f2d59bf2d4e2358549d6fad0
korir desmond <korirdesmond@MacBook-Air.local>
2023-06-08 12:52:36 +0300

    Pass price feed address depending on the network that we are on

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.18",
+    solidity: "0.8.9",
+    defaultNetwork: "hardhat"
 }
